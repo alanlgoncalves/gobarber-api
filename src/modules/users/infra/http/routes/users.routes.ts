d16ec167c44b9dcd9bccbba1a0b1c06e1873296e
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-05-17 18:01:51 -0300

    Validate request data with Celebrate

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -7,13 +7,24 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
