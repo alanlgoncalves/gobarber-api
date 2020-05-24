531440d1c3832911379c1fd27a4317ad2a71794b
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-05-24 16:39:39 -0300

    Add S3 storage provider

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -3,14 +3,14 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from '@config/upload';
+import { celebrate, Joi, Segments } from 'celebrate';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
-import { celebrate, Joi, Segments } from 'celebrate';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
