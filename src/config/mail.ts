ec5f903ec861112816d1c5085ae942dc68c4e43b
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-05-24 11:51:28 -0300

    Add SES mail provider

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'developer@alansantos.dev',
+      name: 'Developer Alan Santos',
+    },
+  },
+} as IMailConfig;
